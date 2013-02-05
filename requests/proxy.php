0a4ea9315f68c3f0fa298c0f1c6e34cf9b9daf1d
Jamie Chung <me@jamiechung.me>
2013-02-05 13:05:37 -0500

    Updating jquery version and proxy to support caching.

--- requests/proxy.php
+++ requests/proxy.php
@@ -1,14 +1,36 @@
 <?php
 
-$url = $_GET['__url'];
+/**
+* JSON proxy for localhost development.
+* @author Jamie Chung
+* @email jfchung@vt.edu
+*/
 
 header('Content-type: application/json');
 
-if ( strlen($url) > 0 && $contents = file_get_contents($url) )
+$contents = json_encode(array());
+
+$url = $_GET['__url'];
+if ( $parsed = parse_url($url) )
 {
-	echo $contents;
+	if ( $parsed['host'] == 'vtrelay.alwaysdata.net' )
+	{
+		$cached_url = 'cached/'.md5($url);
+
+		if ( file_exists($cached_url) )
+		{
+			$contents = file_get_contents($cached_url);
+		}
+		else
+		{
+			$contents = file_get_contents($url);
+
+			$fh = fopen($cached_url, 'w+');
+			fwrite($fh, $contents);
+			fclose($fh);
+		}
+	}
 }
-else
-{
-	echo json_encode(array());
-}
+
+echo $contents;
+
